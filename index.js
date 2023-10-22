218df8afbea04e4c9bb1b73bfdc0398036d61c39
MelanieHuangg <melanie.huang0306@gmail.com>
2023-10-22 18:10:51 -0400

    index.js

--- index.js
+++ index.js
@@ -84,19 +84,60 @@ const titles = [
   "Little Women",
 ];
 
+
 //Array of authors and the book they wrote
 //"--- wrote --- in ---"
 
+/* Notes:
+- parameter (inside the purple ()) = the input of the function
+- {} are the instructions; the forEach function will run everything inside the brace 
+- assign map to a new const; ex: i assigned map to the const newArr */
+
+const newArr = books.map((description) => {
+  return (description.authorFirst + " " + description.authorLast + " wrote " + description.name + " in " + description.publishDate)
+
+});
+console.log(newArr);
+
+
 //Sort books from oldest to most recent
+
+const orderedBooks = books.sort(function (book1, book2) {
+  return book1.publishDate - book2.publishDate;
+});
+console.log(orderedBooks);
+
 
 //sort books alphabetically
 
+const alphabetically = titles;
+alphabetically.sort();
+console.log(alphabetically);
+
+
 //Find who wrote War and Peace
+
+/* Notes:
+one equal sign for assignment; two equal signs for checking */
+const warAndPeace = books.find((book) => book.name === "War and Peace");
+console.log(warAndPeace.authorFirst + " " + warAndPeace.authorLast);
+
 
 //how many books were written before 1900?
 
+const before1900 = books.filter((book) => book.publishDate < 1900);
+console.log(before1900)
+
+
 //was there at least one book published within the last 100 years?
+
+
 
 //was every book published within the last 100 years?
 
+
+
 //print a list of books that "includes" the genre historical
+
+const historical = books.filter((book) => book.genre.includes("historical"));
+console.log(historical)
